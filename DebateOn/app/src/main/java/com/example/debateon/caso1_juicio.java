809bb24be6dfe737ce5ecf6d4220ed6b8c830319
BernardoG <A00570682@itesm.mx>
2021-11-22 23:21:42 -0600

    Added finalizar btn functionality and dynamic interface with btns

--- DebateOn/app/src/main/java/com/example/debateon/caso1_juicio.java
+++ DebateOn/app/src/main/java/com/example/debateon/caso1_juicio.java
@@ -14,8 +14,12 @@ public class caso1_juicio extends AppCompatActivity {
     Control_Info control_info;
     TextView textViewCaption;
     TextView textViewRefuta;
+    TextView postura;
     ImageView msgBox2;
     Button btnContinuar;
+    Button btnFavor;
+    Button btnContra;
+    Button btnfin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +30,12 @@ public class caso1_juicio extends AppCompatActivity {
 
         textViewCaption = findViewById(R.id.txt_juicio_caption);
         textViewRefuta = findViewById(R.id.txt_juicio_refuta);
+        postura = findViewById(R.id.textView4);
         msgBox2 = findViewById(R.id.msgBox2);
         btnContinuar = findViewById(R.id.btn_caso1_continuar);
+        btnFavor = findViewById(R.id.btn_favor_juicio);
+        btnContra = findViewById(R.id.btn_contra_juicio);
+        btnfin = findViewById(R.id.btn_finalizar);
 
         changeText();
 
@@ -38,6 +46,12 @@ public class caso1_juicio extends AppCompatActivity {
                 control_info.postura = 1;
                 control_info.decision_id += '1';
                 control_info.contador_niveles++;
+                msgBox2.setVisibility(View.INVISIBLE);
+                textViewRefuta.setVisibility(View.INVISIBLE);
+                btnContinuar.setVisibility(View.VISIBLE);
+                postura.setVisibility(View.INVISIBLE);
+                btnFavor.setVisibility(View.INVISIBLE);
+                btnContra.setVisibility(View.INVISIBLE);
                 changeText();
             }
         });
@@ -49,6 +63,12 @@ public class caso1_juicio extends AppCompatActivity {
                 control_info.postura = 0;
                 control_info.decision_id += '0';
                 control_info.contador_niveles++;
+                msgBox2.setVisibility(View.INVISIBLE);
+                textViewRefuta.setVisibility(View.INVISIBLE);
+                btnContinuar.setVisibility(View.VISIBLE);
+                postura.setVisibility(View.INVISIBLE);
+                btnFavor.setVisibility(View.INVISIBLE);
+                btnContra.setVisibility(View.INVISIBLE);
                 changeText();
             }
         });
@@ -60,6 +80,23 @@ public class caso1_juicio extends AppCompatActivity {
                 msgBox2.setVisibility(View.VISIBLE);
                 textViewRefuta.setVisibility(View.VISIBLE);
                 btnContinuar.setVisibility(View.INVISIBLE);
+                if (control_info.contador_niveles != 2){
+                    postura.setVisibility(View.VISIBLE);
+                    btnFavor.setVisibility(View.VISIBLE);
+                    btnContra.setVisibility(View.VISIBLE);
+                }
+                else if (control_info.contador_niveles == 2){
+                    btnfin.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
+
+        //Finalizar
+        findViewById(R.id.btn_finalizar).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
@@ -70,27 +107,34 @@ public class caso1_juicio extends AppCompatActivity {
             switch (control_info.contador_niveles) {
                 case 0:
                     control_info.respuesta_usuario = getString(R.string.respuesta_ted_contra_0_0) ;
+                    control_info.contra_arg = getString(R.string.refuta_ted_contra_0_0);
                     break;
                 case 1:
                     if (control_info.postura == 0){
                         control_info.respuesta_usuario = getString(R.string.respuesta_ted_contra_1_0) ;
+                        control_info.contra_arg = getString(R.string.refuta_ted_contra_1_0);
                     }
                     else if (control_info.postura == 1) {
                         control_info.respuesta_usuario = getString(R.string.respuesta_ted_contra_1_1) ;
+                        control_info.contra_arg = getString(R.string.refuta_ted_contra_1_1);
                     }
                     break;
                 case 2:
                     if (control_info.decision_id.equals("00") && control_info.postura == 0){
                         control_info.respuesta_usuario = getString(R.string.respuesta_ted_contra_2_0);
+                        control_info.contra_arg = getString(R.string.refuta_ted_contra_2_0);
                     }
                     else if (control_info.decision_id.equals("01") && control_info.postura == 1){
                         control_info.respuesta_usuario = getString(R.string.respuesta_ted_contra_2_1);
+                        control_info.contra_arg = getString(R.string.refuta_ted_contra_2_1);
                     }
                     else if (control_info.decision_id.equals("10") && control_info.postura == 0){
                         control_info.respuesta_usuario = getString(R.string.respuesta_ted_contra_2_2);
+                        control_info.contra_arg = getString(R.string.refuta_ted_contra_2_2);
                     }
                     else if (control_info.decision_id.equals("11") && control_info.postura == 1){
                         control_info.respuesta_usuario = getString(R.string.respuesta_ted_contra_2_3);
+                        control_info.contra_arg = getString(R.string.refuta_ted_contra_2_3);
                     }
                     break;
 
@@ -100,35 +144,42 @@ public class caso1_juicio extends AppCompatActivity {
             switch (control_info.contador_niveles) {
                 case 0:
                     control_info.respuesta_usuario = getString(R.string.respuesta_ted_favor_0_0) ;
-
+                    control_info.contra_arg = getString(R.string.refuta_ted_favor_0_0);
                     break;
                 case 1:
                     if (control_info.postura == 0){
                         control_info.respuesta_usuario = getString(R.string.respuesta_ted_favor_1_0) ;
+                        control_info.contra_arg = getString(R.string.refuta_ted_favor_1_0);
                     }
                     else if (control_info.postura == 1) {
                         control_info.respuesta_usuario = getString(R.string.respuesta_ted_favor_1_1) ;
+                        control_info.contra_arg = getString(R.string.refuta_ted_favor_1_1);
                     }
 
                     break;
                 case 2:
                     if (control_info.decision_id.equals("00") && control_info.postura == 0){
                         control_info.respuesta_usuario = getString(R.string.respuesta_ted_favor_2_0);
+                        control_info.contra_arg = getString(R.string.refuta_ted_favor_2_0);
                     }
                     else if (control_info.decision_id.equals("01") && control_info.postura == 1){
                         control_info.respuesta_usuario = getString(R.string.respuesta_ted_favor_2_1);
+                        control_info.contra_arg = getString(R.string.refuta_ted_favor_2_1);
                     }
                     else if (control_info.decision_id.equals("10") && control_info.postura == 0){
                         control_info.respuesta_usuario = getString(R.string.respuesta_ted_favor_2_2);
+                        control_info.contra_arg = getString(R.string.refuta_ted_favor_2_2);
                     }
                     else if (control_info.decision_id.equals("11") && control_info.postura == 1){
                         control_info.respuesta_usuario = getString(R.string.respuesta_ted_favor_2_3);
+                        control_info.contra_arg = getString(R.string.refuta_ted_favor_2_3);
                     }
                     break;
             }
         }
         Log.i("testing:", control_info.postura_inicial + " " + control_info.postura + " "+ control_info.contador_niveles + " " + control_info.decision_id + " " + control_info.respuesta_usuario);
         textViewCaption.setText(control_info.respuesta_usuario.toString());
+        textViewRefuta.setText(control_info.contra_arg.toString());
 
 //        Handler handler = new Handler();
 //        handler.postDelayed(new Runnable() {
